92a8a2294780d758282ab254090b7cbbe03e753a
Irem Sena GOLCUK <100692728+senagolcuk@users.noreply.github.com>
2023-05-24 19:10:26 +0300

    Delete basic_scripts directory

--- basic_scripts/pointcloud_container.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-source ~/projects/autoware/install/setup.bash 
-ros2 launch autoware_launch pointcloud_container.launch.py use_multithread:=true container_name:=pointcloud_container
