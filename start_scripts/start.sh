36aae631bfd0bef719f75327d70c7e7e62a8ab7b
Leo Dive <project@leodrive.ai>
2023-09-19 12:05:33 +0300

    fix: enable pc container

--- start_scripts/start.sh
+++ start_scripts/start.sh
@@ -6,7 +6,7 @@ DIR_LAUNCH_SCRIPTS=${DIR_MAIN}""
 
 array_path_scripts=()
 array_path_scripts+=("/$DIR_LAUNCH_SCRIPTS/ntrip_client.sh") 
-#array_path_scripts+=("/$DIR_LAUNCH_SCRIPTS/pointcloud_container.sh") 
+array_path_scripts+=("/$DIR_LAUNCH_SCRIPTS/pointcloud_container.sh") 
 array_path_scripts+=("/$DIR_LAUNCH_SCRIPTS/aw_launch.sh")
 array_path_scripts+=("/$DIR_LAUNCH_SCRIPTS/localization_error_plugin.sh") 
 array_path_scripts+=("/$DIR_LAUNCH_SCRIPTS/vcu_driver.sh") 
@@ -19,3 +19,5 @@ mate-terminal --tab --command="bash -ic 'echo $cmd1 >> $HOME/.bash_history; trap
 for path_script in "${array_path_scripts[@]}"; do
     run_in_tab "${path_script}"
 done
+
+
