89d7946a6a27c9f57cb7c0aa651f044a4cb72443
Nilay Sener <nilay@leodrive.ai>
2024-10-24 13:04:37 +0300

    update script and add .env file
    Signed-off-by: NilaySener <nilay@leodrive.ai>

--- start_scripts/autoware-start.sh
+++ start_scripts/autoware-start.sh
@@ -10,19 +10,26 @@ export DISPLAY
 echo DISPLAY="$DISPLAY"
 
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-# shellcheck disable=SC1083
 export CYCLONEDDS_URI=/home/golf/cyclonedds.xml
 export RCUTILS_COLORIZED_OUTPUT=1
 export RCUTILS_CONSOLE_OUTPUT_FORMAT='[{severity} {time}] [{name}]: {message} ({function_name}() at {file_name}:{line_number})'
 export PATH="/usr/local/cuda/bin:$PATH"
 export LD_LIBRARY_PATH="/usr/local/cuda/lib64:$LD_LIBRARY_PATH"
 
-source ~/projects/autoware.golf.dev/install/setup.bash
 
-echo Running ROS 2 launch.
+# Source environment-specific variables
+if [ -f /home/golf/autoware.env ]; then
+  source /home/golf/autoware.env
+else
+  echo "Environment file not found!"
+  exit 1
+fi
+source "$AUTOWARE_PROJECT_PATH/install/setup.bash"
 
+echo "Running ROS 2 launch."
+
+# Use variables from .env file
 ros2 launch autoware_launch autoware.launch.xml \
-  map_path:=/home/golf/files/autoware_map/ytu-demo/ \
-  vehicle_model:=golf_vehicle \
-  sensor_model:=golf_sensor_kit
-
+  map_path:="$MAP_PATH" \
+  vehicle_model:="$VEHICLE_MODEL" \
+  sensor_model:="$SENSOR_MODEL"
